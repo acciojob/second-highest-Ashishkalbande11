2bafafb02837ca73857ddc402e7b1c54a5fd0b8d
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-19 09:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+function main() {
+    var n = prompt("Enter the number of elements ");
+    var arr = [];
+    for (let i = 0; i < n; i++) {
+        arr[i] = prompt("Enter element " + (i + 1));
+    }
+    alert(secondHighest(arr));
+}
+
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let high = -Infinity;
+    let secHigh = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = parseInt(arr[i]);
+
+        if (num > high) {
+            secHigh = high;
+            high = num;
+        } else if (num > secHigh && num < high) {
+            secHigh = num;
+        }
+    }
+    if (secHigh === -Infinity) {
+        return -Infinity;
+    }
+    return secHigh;
+}
+
+// Call the main function to start the program
+main();
